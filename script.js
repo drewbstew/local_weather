0935c4e3c2caf1d81018ca7123fc19b5c19fa043
Drew Stewart <drew.b.stew@gmail.com>
2016-04-06 22:10:51 -0700

    working with JSON/AJAX for first time. this could get messy.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-"hello world" 
+// RING RING! WAKE UP, API
+
+var weatherAPI = $.getJSON("api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}");
+
+// GLOBAL VARIABLES
+
+var lat;
+var long;
+
+// EVENTS
+
+function getWeather(la,lo){
+  la = lat;
+  lo = long;
+}
+
+function getCoords(lat,long){
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+    });
+  }
+}
+
+// DOCUMENT READY
+
+$(document).ready(function(){
+  getWeather();
+});
