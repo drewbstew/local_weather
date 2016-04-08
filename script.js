e9790d66b0568cf1e2825f37a03047a06ed2e57a
Drew Stewart <drew.b.stew@gmail.com>
2016-04-08 11:16:18 -0700

    more testing

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ $(function() {
     dataType: 'json',
     url: "example.json",
     success: function(info) {
-      weatherAPI = JSON.parse(info);
+      weatherAPI = info;
     }
   });
   getCoords();
