d8241f8f25294ab2a20ad342fb4d7cb48653dce0
Drew Stewart <drew.b.stew@gmail.com>
2016-04-11 16:11:24 -0700

    more testing, but hope is low

--- script.js
+++ script.js
@@ -120,13 +120,15 @@ function setForecastWeather() {
 
 function setForecastDailyWeather() {
   var forecastData = weatherAPI.daily.data;
-  $forecastDays.each(function(i) {
+  $forecastDays.each(function() {
+    var i = 0;
     var conditions = formatConditions(forecastData[i].icon);
     this.find("h4").html(findDay(forecastData[i].time));
     this.find("p").html(forecastData[i].summary);
     this.find(".row .info-col .ul .forecast-temp-max").html("High: " + formatTemp(forecastData[i].temperatureMax).toString());
     this.find(".row .info-col .ul .forecast-temp-min").html("Low: " + formatTemp(forecastData[i].temperatureMin).toString());
     changeIcon(this.find(".row .info-col .weather-icon"), conditions);
+    i++;
   });
 }
 
