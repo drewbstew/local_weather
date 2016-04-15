dcd1c94e35d6616459bf8d437f7a34b503017a60
Drew Stewart <drew.b.stew@gmail.com>
2016-04-15 10:55:44 -0700

    adding Copy to grunt so I can copy files to apache server dir easily

--- gruntfile.js
+++ gruntfile.js
@@ -4,11 +4,19 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all: ['*.js']
+    },
+    copy: {
+      main: {
+        expand: true,
+        src: '**/*',
+        dest: '../../../../../../../xampp/htdocs/local_weather'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'copy']);
 
 };
